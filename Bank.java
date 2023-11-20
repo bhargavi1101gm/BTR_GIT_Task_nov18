fd7403996ed0234c6cbbc95cc2c398ad7366d43d
bhargavi <bhargavi02@gmail.com>
2023-11-20 15:58:05 +0000

    All the files are added to branch java

---
+++ Bank.java
@@ -0,0 +1,33 @@
+class Bank
+{
+ public static void main(String[] args)
+ {
+ 	String acc_name = "Bhargavi";
+ 	long mob_num = 9876543210l;
+ 	long adhar = 541652711508l;
+ 	String address = " #199, 1st stage, Basaveshwaranagar, Bangalore";
+ 	String email_id = "poojarcs107@gmail.com";
+ 	double bal = 10000.00;
+ 	char gender = 'F';
+ 	byte age = 23;
+ 	String DOB = "01/NOV/2000";
+ 	String bank_name = "SBI";
+ 	String branch_name = "Basaveshwaranagar";
+ 	String IFSC_code = "SBIN0080";
+ 	String Account_type = "Savings";
+
+ 	System.out.println("Account Holder Name : " +acc_name);
+ 	System.out.println("Mobile Number : " +mob_num);
+ 	System.out.println("Aadhar Number : " +adhar);
+ 	System.out.println("Address : "+address);
+ 	System.out.println("EMAIL : "+email_id);
+ 	System.out.println("Balance :"+bal);
+ 	System.out.println("Gender :" +gender);
+ 	System.out.println("Age : "+age);
+ 	System.out.println("DOB : "+DOB);
+ 	System.out.println("Bank Name : " +bank_name);
+ 	System.out.println("Branch Name :" +branch_name);
+ 	System.out.println("IFSC CODE :"+IFSC_code);
+ 	System.out.println("ACCOUNT TYPE : "+Account_type);
+ }
+}
---
+++ employee.java
@@ -0,0 +1,16 @@
+class employee
+{
+	public static void main(String[] args) {
+		String emp_name = "Bhargavi";
+		int emp_id = 1415;
+		String emp_email_id = "bhargavi@gmail.com";
+		long emp_mobile_no = 9754145541l;
+		double emp_salary = 12000.00;
+
+		System.out.println("Name :   " 		+emp_name);
+		System.out.println("ID : " 			+emp_id);
+		System.out.println("EMAIL ID :  "	+emp_email_id);
+		System.out.println("MOBILE NO. : "  +emp_mobile_no);
+		System.out.println("SALARY : " 		+emp_salary);
+	}
+}
